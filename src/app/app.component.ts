840e91c91f1c96baa2b0c80991902d1e8433a0fc
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-15 15:08:18 +0530

    integrated dbn plugin dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import '@databrainhq/plugin/web';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  public token: string = '';
+  public dashboardId: string = '';
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(){
+    this.token = this.activatedRoute.snapshot.paramMap.get('token') || '';
+    this.dashboardId = this.activatedRoute.snapshot.paramMap.get('dashboardId') || '';
+  }
 }
